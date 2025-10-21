48457852ebc05d9d830dfdc747f58a65f1b57733
Josh <j.haabicht@gmail.com>
2025-10-21 13:26:58 +0200

    WIP

---
+++ DonaldOS/FileSystem.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sys = Cosmos.System;
+
+namespace DonaldOS
+{
+    enum FileSystemElementTypes
+    {
+        Dirs,
+        Files,
+        All
+    }
+    internal class FileSystem
+    {
+        public FileStream createFile(string path, string filename)
+        {
+            if (path.Last()  != '\\')
+            {
+                path.Append('\\');
+            }
+
+            FileStream file_stream = null;
+            try
+            {
+                 file_stream = File.Create(path + filename);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            return file_stream;
+        }
+
+        public void listDir(string path, int recursionLevel = 0, bool recursive = false, FileSystemElementTypes elementsToShow = FileSystemElementTypes.All, string filterString = null)
+        { // TODO: rekursive Aufrufe geben nicht gleich alle Elemente aus, sondern befüllen nur nach und nach ein Array
+          // von einer eigenen FileSystemElement-Klasse, in dem die auszugebenden Elemente drinstehen
+          // Vorteil: bessere Ausgabe bei Angabe eines FilterStrings (würde Zurückgehen im Array und Löschen ermöglichen)
+            Console.WriteLine("DEBUG");
+            if (!Directory.Exists(path))
+            {
+                throw new Exception("Path does not exist");
+            }
+
+            if (!path.EndsWith('\\'))
+            {
+                path.Append('\\');
+            }
+
+            if (elementsToShow == FileSystemElementTypes.All || elementsToShow == FileSystemElementTypes.Files)
+            { 
+                string[] fileNames = Directory.GetFiles(path);
+                foreach (string fileName in fileNames)
+                {
+                    if (fileName.Contains(filterString))
+                    {
+                        Console.WriteLine(path + fileName);
+                    }
+                }
+            }
+            if (elementsToShow == FileSystemElementTypes.All || elementsToShow == FileSystemElementTypes.Dirs)
+            {
+                string[] dirNames = Directory.GetDirectories(path);
+                foreach (string dirName in dirNames)
+                {
+                    if (dirName.Contains(filterString))
+                    {
+                        Console.WriteLine(path + dirName + '\\');
+                    }
+                    listDir(path + dirName, recursionLevel + 1, recursive, elementsToShow, filterString);
+                }
+            }
+        }
+    }
+}
--- DonaldOS/Kernel.cs
+++ DonaldOS/Kernel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text;
 using Sys = Cosmos.System;
 
@@ -7,18 +9,98 @@ namespace DonaldOS
 {
     public class Kernel : Sys.Kernel
     {
+        private FileSystem fs;
+        Sys.FileSystem.CosmosVFS vfs = new Cosmos.System.FileSystem.CosmosVFS();
 
+        private string currentPath = @"0:\";
+
+        private SortedDictionary<String, Func<String[], bool>> CommandDict = new SortedDictionary<String, Func<String[], bool>>();
         protected override void BeforeRun()
         {
+            Sys.FileSystem.VFS.VFSManager.RegisterVFS(vfs);
             Console.WriteLine("Cosmos booted successfully. Type a line of text to get it echoed back.");
         }
 
         protected override void Run()
         {
+         
             Console.Write("Input: ");
             var input = Console.ReadLine();
-            Console.Write("Text typed: ");
-            Console.WriteLine(input);
-        }
+            
+            string[] args = input.Split(' ');
+            switch (args[0])
+            {
+                case "touch":
+                    {
+                        fs.createFile(currentPath, args[1]); 
+                        break;
+                    }
+                case "ls":
+                    {
+                        string path = currentPath;
+                        bool recursive = false;
+                        FileSystemElementTypes elementTypes = FileSystemElementTypes.All;
+                        string filterString = null;
+                        for (int i = 1; i < args.Length; i++)
+                        {
+                            switch (args[i])
+                            {
+                                case "--path":
+                                    {
+                                        path = args[i + 1];
+                                        i++;
+                                        break;
+                                    }
+                                case "--recursive":
+                                    {
+                                        recursive = true;
+                                        break;
+                                    }
+                                case "--dirs":
+                                    {
+                                        elementTypes = FileSystemElementTypes.Dirs;
+                                        break;
+                                    }
+                                case "--files":
+                                    {
+                                        elementTypes = FileSystemElementTypes.Files;
+                                        break;
+                                    }
+                                case "--filter":
+                                    {
+                                        filterString = args[i + 1];
+                                        i++;
+                                        break;
+                                    }
+                                default:
+                                    {
+                                        Console.WriteLine("ls: Unknown flag " + args[i] + "\nUse ls -h for help.");
+                                        return;
+                                    }
+                            }
+                        }
+                        try
+                        {
+                            Console.WriteLine("DEBUGOUTSIDE");
+                            fs.listDir(currentPath, 0, recursive, elementTypes, filterString);
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("ls: " + e.ToString());
+                        }
+                        break;
+                    }
+                case "rm":
+                    {
+                        File.Delete(@"0:\" +  args[1]);
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine(input);
+                        break;
+                    }
+            }
+            }
     }
 }
